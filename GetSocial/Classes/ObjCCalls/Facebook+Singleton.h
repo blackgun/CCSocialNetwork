b995ad17c6fa2dfa560e84c949481887e58b315d
Piperoman <mrpiperoman.hot@gmail.com>
2012-09-22 15:02:22 +0200

    Restart all

--- GetSocial/Classes/ObjCCalls/Facebook+Singleton.h
+++
@@ -1,8 +0,0 @@
-#import "FBConnect.h"
-#define kFBAccessTokenKey  @"FBAccessTokenKey"
-#define kFBExpirationDateKey  @"FBExpirationDateKey"
-
-@interface Facebook (Singleton) <FBSessionDelegate>
-- (void)authorize;
-+ (Facebook *)shared;
-@end
--- GetSocial/Classes/ObjCCalls/MailViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <MessageUI/MessageUI.h>
-
-@interface MailViewController : UIViewController <MFMailComposeViewControllerDelegate>
-
-- (IBAction)openMail:(id)sender;
-
-@end
