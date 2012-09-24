885315ccdfd1e745598efebf5e2545be923fb131
Piperoman <mrpiperoman.hot@gmail.com>
2012-09-24 22:11:34 +0200

    No trash

--- GetSocial/proj.android/bin/classes/ObjCCalls/Facebook+Singleton.h
+++
@@ -1,9 +0,0 @@
-#import "FBConnect.h"
-#define kFBAccessTokenKey  @"FBAccessTokenKey"
-#define kFBExpirationDateKey  @"FBExpirationDateKey"
-
-@interface Facebook (Singleton) <FBSessionDelegate, FBDialogDelegate>
-- (void)authorize;
--(void) postOnFB:(int)score;
-+ (Facebook *)shared;
-@end
--- GetSocial/proj.android/obj/local/armeabi/objs-debug/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dx_static/support/ccUtils.o: \
- /home/piperoman/Librerias/cocos2d-2.0-rc2-x-2.0.2/cocos2dx/support/ccUtils.cpp \
- /home/piperoman/Librerias/cocos2d-2.0-rc2-x-2.0.2/cocos2dx/support/ccUtils.h
-
-/home/piperoman/Librerias/cocos2d-2.0-rc2-x-2.0.2/cocos2dx/support/ccUtils.h:
