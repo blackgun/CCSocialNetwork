3cab71ffd8ac2a463d670174fd8ea641b0cd2534
Piperoman <mrpiperoman.hot@gmail.com>
2012-09-26 23:45:19 +0200

    Integrate facebook

--- GetSocial/proj.android/src/tutorial/getsocial/meizpipero/GetSocial.java
+++ GetSocial/proj.android/src/tutorial/getsocial/meizpipero/GetSocial.java
@@ -23,25 +23,68 @@ THE SOFTWARE.
 ****************************************************************************/
 package tutorial.getsocial.meizpipero;
 
+import tutorial.getsocial.meizpipero.FacebookConnector;
+import com.facebook.android.Facebook;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Date;
+
 import org.cocos2dx.lib.Cocos2dxActivity;
 import org.cocos2dx.lib.Cocos2dxEditText;
 import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 import org.cocos2dx.lib.Cocos2dxRenderer;
 
+import tutorial.getsocial.meizpipero.SessionEvents;
+
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ConfigurationInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.view.ViewGroup;
 
-public class GetSocial extends Cocos2dxActivity{
+public class GetSocial extends Cocos2dxActivity
 
-	protected void onCreate(Bundle savedInstanceState){
+{
+
+	private static final String FACEBOOK_APPID = "317764948310799";
+	private static final String FACEBOOK_PERMISSION = "publish_stream";
+	private static final String TAG = "FacebookSample";
+	private static final String MSG = "Message from FacebookSample";
+	
+	private final Handler mFacebookHandler = new Handler();
+	private TextView loginStatus;
+	private FacebookConnector facebookConnector;
+	
+	final Runnable mUpdateFacebookNotification = new Runnable() 
+	{
+        public void run() 
+        {
+        	Toast.makeText(getBaseContext(), "Facebook updated !", Toast.LENGTH_LONG).show();
+        }
+    };
+    
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
-		
-		if (detectOpenGLES20()) {
+		//
+		this.facebookConnector = new FacebookConnector(FACEBOOK_APPID, this, getApplicationContext(), new String[] {FACEBOOK_PERMISSION});
+		//
+		/* POST
+		 * postMessage();
+		 */
+		/* CLEAR
+		 * clearCredentials();
+            updateLoginStatus();
+		 */
+		if (detectOpenGLES20()) 
+		{
 			// get the packageName,it's used to set the resource path
 			String packageName = getApplication().getPackageName();
 			super.setPackageName(packageName);
@@ -105,4 +148,65 @@ public class GetSocial extends Cocos2dxActivity{
      static {
          System.loadLibrary("game");
      }
+     
+     //
+     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+ 		this.facebookConnector.getFacebook().authorizeCallback(requestCode, resultCode, data);
+ 	}
+     
+     public void updateLoginStatus() {
+ 		loginStatus.setText("Logged into Twitter : " + facebookConnector.getFacebook().isSessionValid());
+ 	}
+ 	
+
+ 	private String getFacebookMsg() {
+ 		return MSG + " at " + new Date().toLocaleString();
+ 	}	
+ 	
+ 	public void postMessage() {
+ 		
+ 		if (facebookConnector.getFacebook().isSessionValid()) {
+ 			postMessageInThread();
+ 		} else {
+ 			SessionEvents.AuthListener listener = new SessionEvents.AuthListener() {
+ 				
+ 				@Override
+ 				public void onAuthSucceed() {
+ 					postMessageInThread();
+ 				}
+ 				
+ 				@Override
+ 				public void onAuthFail(String error) {
+ 					
+ 				}
+ 			};
+ 			SessionEvents.addAuthListener(listener);
+ 			facebookConnector.login();
+ 		}
+ 	}
+
+ 	private void postMessageInThread() {
+ 		Thread t = new Thread() {
+ 			public void run() {
+ 		    	
+ 		    	try {
+ 		    		facebookConnector.postMessageOnWall(getFacebookMsg());
+ 					mFacebookHandler.post(mUpdateFacebookNotification);
+ 				} catch (Exception ex) {
+ 					Log.e(TAG, "Error sending msg",ex);
+ 				}
+ 		    }
+ 		};
+ 		t.start();
+ 	}
+
+ 	private void clearCredentials() {
+ 		try {
+ 			facebookConnector.getFacebook().logout(getApplicationContext());
+ 		} catch (MalformedURLException e) {
+ 			e.printStackTrace();
+ 		} catch (IOException e) {
+ 			e.printStackTrace();
+ 		}
+ 	}
 }
